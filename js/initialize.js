57721c2e922305c0aef2d868aa1fe75cfbcb8709
Pierrick Moreau <pierrick.moreau.etud@hotmail.fr>
2015-01-20 18:15:28 +0100

    using OSM instead of Google Maps

--- js/initialize.js
+++ js/initialize.js
@@ -1,8 +1,12 @@
+var map, myPosition, myPositionRadius;
+var mapInitialized = false;
+var myPosition;
+
 function init()
 {
 	if (navigator.geolocation)
 	{
-		navigator.geolocation.watchPosition(show, errorHandler, {enableHighAccuracy:true, timeout: 20000, maximumAge: 0});
+		navigator.geolocation.getCurrentPosition(showMyPosition, errorHandler, {enableHighAccuracy:true, timeout: 50000, maximumAge: 0});
 	}
 	else 
 	{
@@ -10,51 +14,53 @@ function init()
 	}
 }
 
-function show(position)
+function showMyPosition(position)
 {
-	var myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-	
-	var mapOptions = {
-		zoom: 13,
-		center: myLatLng,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	
-	var $content = $("#map div:jqmData(role=content)");
-	$content.height(screen.height);
-	
-	var map = new google.maps.Map($content[0], mapOptions);
-	
-	$.mobile.changePage($("#map"));
-
-	var myPositionMarker = new google.maps.Marker({
-		position: myLatLng,
-		map: map,
-		icon: 'img/geoloc.png',
-		animation : google.maps.Animation.DROP
-	});
-	
-	var populationOptions = {
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 1,
-      fillColor: '#F2F2F2',
-      fillOpacity: 0.35,
-      map: map,
-      center: myLatLng,
-      radius: 50*100
-    };
-	
-	cityCircle = new google.maps.Circle(populationOptions);
-
-	  var widgetDiv = document.getElementById('text');
- 	 map.controls[google.maps.ControlPosition.TOP_LEFT].push(widgetDiv);
-
+	var latLng = L.latLng(position.coords.latitude, position.coords.longitude);
+	if (!mapInitialized)
+	{
+		map = L.map('map',
+		{
+			center: latLng,
+			zoom: 13
+		});
+		
+		L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+			subdomains: ['otile1','otile2','otile3','otile4']
+		}).addTo( map );
+		
+		mapInitialized = true;
+		myPosition = L.marker(latLng).addTo(map);
+		myPositionRadius = L.circle(latLng, 1000*5, {color: 'red', fillColor: '#f03',fillOpacity: 0.5}).addTo(map);
+	}
+	else
+	{
+		myPosition.setLatLng(latLng).update();
+		myPositionRadius.setLatLng(latLng).update();
+	}
 }
 
 function errorHandler(error)
 {
-	alert("Error while retrieving current position. Error code: " + error.code + ",Message: " + error.message);
+	switch(error.code)
+	{
+        case error.TIMEOUT:
+            init();
+            break;
+
+        case error.PERMISSION_DENIED:
+            alert("Erreur : L'application n'a pas l'autorisation d'utiliser les ressources de geolocalisation.");
+            break;
+
+        case error.POSITION_UNAVAILABLE:
+            alert("Erreur : La position n'a pas pu être déterminée.");
+            break;
+
+        default:
+            alert("Erreur "+error.code+" : "+error.message);
+            break;
+    }
 }
 
 $(document).ready(function(){

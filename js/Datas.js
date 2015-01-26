2f26cab9f480035d475cf3f41feb8f24afb63421
Antoine Mazelin <antoine.mazelin@free.fr>
2015-01-26 19:42:42 +0100

    récupération d'un tableau avec toutes les données des stations

--- js/Datas.js
+++ js/Datas.js
@@ -1,8 +1,7 @@
-$(document).ready(function(){
+// Fonction qui retourne un tableau de stations les plus proches de la position actuelle (rayon de 5 km)
+function searchStations(positionActuelle){
 
-	$('#target').prop('disabled', true);
-
-	// Function qui va rÃ©cupÃ©rer le fichier ZIP et le dÃ©compresser pour en sortir le XML
+	// Function qui va récupérer le fichier ZIP et le décompresser pour en sortir le XML
 	JSZipUtils.getBinaryContent('http://localhost/datas', function(err, data) {
 
 		if(err) {
@@ -15,45 +14,75 @@ $(document).ready(function(){
 		// Liste des fichiers du zip (il n'y en a qu'un)
 		var files = zip.files;
 
-		// Obtention de la seule clÃ© de la liste
+		// Obtention de la seule clé de la liste
 		var filename = null;
 		for(var key in files){
 			filename = key;
 			break;
 		}
 
-		// Get la seule entrÃ©e du zip : le fichier XML
+		// Get la seule entrée du zip : le fichier XML
 		var xml = zip.file(filename);
-		// Et on rÃ©cupÃ¨re son contenu
-		var xmlFile = xml.asText();
 
-		$('#target').prop('disabled', false);
+		// Et on récupère son contenu
+		var xmlFile = xml.asBinary();
 
-
-		$( "#target" ).click(function() {
-
-			// Test avec une valeur
-			// TODO : TESTER AVEC RIS ORANGIS
-			var infosStation = getInfosByLatitudeLongitude(xmlFile,4864729,241893);
-
-			for	(index = 0; index < infosStation.length; index++) {
-   				 
-   				 console.log(infosStation[index].name);
-   				 console.log(infosStation[index].price);
-
-			}
+			// On récupère la liste des stations
+			var infosStation = getInfosByLatitudeLongitude(xmlFile, positionActuelle);
 			
-		});
+			console.log(infosStation);
 	});
-});
+}
 
-// obtention 
-function getInfosByLatitudeLongitude(datas, latitude, longitude){
+// obtention d'une liste de prix/adresse/ville des stations les plus proches (dans un rayon de 10 km)
+function getInfosByLatitudeLongitude(datas, positionActuelle){
 
-	var listOfPetrols = Array();
+	 var listOfStations = Array();
+	 var result = null;
+
+	var value = $(datas).find('pdv').each(
+
+			function(){
+
+				 // get latitude/longitude de chaque entrée
+				 var lat = $(this).attr('latitude');
+				 var long = $(this).attr('longitude');
+
+				 // Et on recupère la distance entre la position actuelle et la station à vol d'oiseau
+				 result = positionActuelle.distanceTo([lat/100000, long/100000])/1000;
+
+				 // Moins de 10km ? OK on ajoute la station dans la liste
+				 if( result <= 5){
+
+				 	// Initialisation de la liste des prix 
+				 	listOfPetrols = Array();
+
+				 	// Parcours des prix
+				 	$(this).find("prix").each(
+				 		function(){
+
+				 			// Ajout de chaque Produit dans la liste
+				 			listOfPetrols.push( {'nom' : $(this).attr('nom'), 'prix' : $(this).attr('valeur')/1000} );
+
+				 		}
+				 	);
+
+				 	// Récupération de la ville et de l'adresse
+				 	var ville = $(this).find("adresse").text();
+				 	var adresse = $(this).find("ville").text();
+
+				 	listOfStations.push( 
+				 		{'latitude' : lat, 'longitude' : long, 'ville' : ville, 'adresse' : adresse, 'prix' : listOfPetrols} )
+
+				 } 
+			}
+		);
+
+	return listOfStations;
+}
 
 	// Requete qui cherche la station en fonction de la longitude et de la latitude
-	var value = $(datas)
+/*	var value = $(datas)
 	.find('pdv[latitude^="'+latitude+'"]pdv[longitude^="'+longitude+'"]').find("prix").each(
 
 		function(){
@@ -66,4 +95,4 @@ function getInfosByLatitudeLongitude(datas, latitude, longitude){
 	);
 
 	return listOfPetrols;
-}
+}*/
